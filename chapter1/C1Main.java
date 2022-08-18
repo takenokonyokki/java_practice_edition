2a7eda4e1c517701dfb7c69ae56f7f0ba63d3088
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-08-18 22:22:08 +0900

    java実践編

---
+++ chapter1/C1Main.java
@@ -0,0 +1,9 @@
+public class C1Main {
+	public static void main(String[] args) {
+		//Objectクラス　→　すべてのクラスは、その継承関係を親へと辿って行くとjava.lang.Objectクラスに行き着く
+		
+		//toString()　→　インスタンスの中身の情報を知りたいならとにかくtoString()を呼び出せばよい。特にデバック作業やログ出力の場面で活躍する。
+
+		
+	}
+}
