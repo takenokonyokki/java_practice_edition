55d45e76b3b63d5201515629ed6b8af80e88529c
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-08-26 23:55:02 +0900

    null安全性

--- chapter2/C2Main1.java
+++ chapter2/C2Main1.java
@@ -1,8 +1,24 @@
+import java.util.*;
+
 public class C2Main1 {
+	//null安全性　⇒　Optionalを使用する
+	public static Optional<String> decorate(String s, char c) { //文字列sを文字cで挟んで装飾するメソッド
+		String r;
+		if (s == null || s.length() == 0) { //文字列sがnullまたは長さ0の場合はnullを返す
+			r = null;
+		} else {
+			r = c + s + c;
+		}
+		return Optional.ofNullable(r);
+	}
+	
 	public static void main(String[] args) {
+		Optional<String> s = decorate("", '*');
+		System.out.println(s.orElse("nullのため処理できません"));
+		
 		C2Pocket<String> p = new C2Pocket<>();
 		p.put("1192");
-		String s = p.get();
-		System.out.println(s);
+		String s2 = p.get();
+		System.out.println(s2);
 	}
 }
