9ebff23b0cb166c3ee859786cff00ac50a1aa0a7
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-08-26 23:15:35 +0900

    列挙型

---
+++ chapter2/Account.java
@@ -0,0 +1,8 @@
+public class Account {
+	private String accountNo;
+	private int zandaka;
+	private AccountType accountType;
+	public Account(String aNo, AccountType aType) { //列挙型を使用
+		//
+	}
+}
---
+++ chapter2/AccountType.java
@@ -0,0 +1,4 @@
+//列挙型(enum type)　⇒　指定した種類の値だけを入れることができる
+public enum AccountType {
+	FUTSU,TOUZA,TEIKI;
+}
